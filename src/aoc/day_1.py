820d8172538bfcd570c95a87783669ac8a659a7c
René Gentzen <rene.gentzen@mittelstand.ai>
2023-12-05 12:28:48 +0000

    Added some code for second part of day 1

--- src/aoc/day_1.py
+++ src/aoc/day_1.py
@@ -1,17 +1,21 @@
+from functools import reduce
 from typing import List
 
 from aoc import INPUT_DIR
 
-DIGITS = {
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
+DIGIT_NAMES = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
+""" DIGITS = {
     1: 1,
     2: 2,
     3: 3,
@@ -21,11 +25,24 @@ DIGITS = {
     7: 7,
     8: 8,
     9: 9,
-}
+} | DIGIT_NAMES """
+
+DIGITS = {
+    "1": "1",
+    "2": "2",
+    "3": "3",
+    "4": "4",
+    "5": "5",
+    "6": "6",
+    "7": "7",
+    "8": "8",
+    "9": "9",
+} | DIGIT_NAMES
 
 
 def extract_numbers_from_line(input: str, include_words: bool = False) -> int:
     if include_words:
+        input = reduce(lambda a, kv: a.replace(*kv), DIGIT_NAMES.items(), input)
         first = next(x for x in input if DIGITS.get(x, None))
         second = next(x for x in reversed(input) if DIGITS.get(x, None))
     else:
@@ -39,7 +56,12 @@ def compute_sum(input: List[int]) -> int:
 
 
 def solve_1(input: List[str]) -> int:
-    tmp = [extract_numbers_from_line(val) for val in input]
+    tmp = [extract_numbers_from_line(val, include_words=False) for val in input]
+    return compute_sum(tmp)
+
+
+def solve_2(input: List[str]) -> int:
+    tmp = [extract_numbers_from_line(val, include_words=True) for val in input]
     return compute_sum(tmp)
 
 
--- tests/test_1.py
+++ tests/test_1.py
@@ -5,8 +5,14 @@ from aoc.day_1 import *
 
 
 @pytest.fixture
-def test_input():
+def test_input_1():
     with open(f"{INPUT_DIR}/1_1_test.txt") as f:
+        return f.readlines()
+
+
+@pytest.fixture
+def test_input_2():
+    with open(f"{INPUT_DIR}/1_2_test.txt") as f:
         return f.readlines()
 
 
@@ -22,8 +28,8 @@ def test_input():
         ["1", 11],
     ],
 )
-def test_number_extraction_with_two_numbers(input, expected):
-    assert extract_numbers_from_line(input) == expected
+def test_number_extraction_with_just_numbers(input, expected):
+    assert extract_numbers_from_line(input, include_words=False) == expected
 
 
 @pytest.mark.parametrize(
@@ -37,5 +43,28 @@ def test_compute_sum(input, expected):
     assert compute_sum(input) == expected
 
 
-def test_solve_1(test_input):
-    assert solve_1(test_input) == 142
+def test_solve_1(test_input_1):
+    assert solve_1(test_input_1) == 142
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        ["1abc23", 13],
+        ["1", 11],
+        ["one", 11],
+        ["one5", 15],
+        ["one1asfa", 11],
+        ["three16a", 36],
+        ["three16aeight", 38],
+        ["two1nine", 29],
+        ["eightwothree", 83],
+    ],
+)
+def test_number_extraction_with_spelled_out_numbers(input, expected):
+    assert extract_numbers_from_line(input, include_words=True) == expected
+
+
+@pytest.mark.skip
+def test_solve_2(test_input_2):
+    assert solve_2(test_input_2) == 281
