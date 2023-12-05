cc4c8d7d7dd21be58fbcbca72d751bfd4bf988f1
René Gentzen <rene.gentzen@mittelstand.ai>
2023-12-05 15:19:32 +0000

    More day 2 part 1 code

--- src/aoc/day_2.py
+++ src/aoc/day_2.py
@@ -1,10 +1,43 @@
-from typing import List
+import re
+from typing import Any, Dict, List, Tuple
 
 from aoc import INPUT_DIR
 
+BALLZ = {
+    "red": 12,
+    "green": 13,
+    "blue": 14,
+}
+
+
+def remove_prefix(s: str) -> str:
+    return re.sub(r"Game \d+: ", "", s)
+
+
+def split_rounds(s: str) -> List[List[str]]:
+    return [[sss.strip() for sss in ss.split(",")] for ss in s.split(";")]
+
+
+def split_counts(input: List[str]) -> List[Tuple[int, str]]:
+    return [(int(d), s) for g in input for d, s in [g.split(" ")]]
+
+
+def count_ballz(input: List[Tuple[int, str]]) -> Dict[str, int]:
+    d = {"red": 0, "green": 0, "blue": 0}
+    for count, color in input:
+        d[color] += count
+    return d
+
+
+def is_round_possible(ballz: Dict[str, int]) -> bool:
+    return all(ballz[k] <= BALLZ[k] for k in ballz.keys())
+
 
 def solve_1(input: List[str]) -> int:
-    return 0
+    s: List[str] = [remove_prefix(l) for l in input]
+    ss = [split_rounds(l) for l in s]
+    sss = [split_counts(ll) for l in ss for ll in l]
+    ssss = [count_ballz(g) for g in sss]
 
 
 def main():
--- tests/test_2.py
+++ tests/test_2.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 import pytest
 
 from aoc import INPUT_DIR
@@ -8,3 +10,35 @@ from aoc.day_2 import *
 def test_input_1():
     with open(f"{INPUT_DIR}/2_1_test.txt") as f:
         return f.readlines()
+
+
+def test_remove_prefix():
+    s = "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+    ss = "6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+    assert remove_prefix(s) == ss
+
+
+def test_split_rounds():
+    s = "6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+    ss = [["6 red", "1 blue", "3 green"], ["2 blue", "1 red", "2 green"]]
+    assert split_rounds(s) == ss
+
+
+def test_split_counts():
+    s = ["6 red", "1 blue", "3 green"]
+    ss = [(6, "red"), (1, "blue"), (3, "green")]
+    assert split_counts(s) == ss
+
+
+def test_count_balls():
+    s = [(6, "red"), (1, "blue"), (3, "green"), (8, "blue")]
+    assert count_ballz(s) == {"red": 6, "blue": 9, "green": 3}
+
+
+def test_is_round_possible():
+    assert solve_1(
+        ["Game 5: 6 red, 1 blue, 3 green, 8 blue; 2 blue, 1 red, 2 green"]
+    ) == [
+        {"red": 6, "blue": 9, "green": 3},
+        {"red": 1, "blue": 2, "green": 2},
+    ]
