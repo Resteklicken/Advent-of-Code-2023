876ea67f310a645cc696770634d5116ce8ea4575
René Gentzen <rene.gentzen@mittelstand.ai>
2023-12-06 15:34:44 +0000

    Added some stuff for day 3 part 2

--- src/aoc/day_3.py
+++ src/aoc/day_3.py
@@ -1,5 +1,5 @@
 import re
-from typing import List, Tuple
+from typing import Any, List, Tuple
 
 from aoc import INPUT_DIR
 
@@ -42,12 +42,51 @@ def solve_1(input: List[str]) -> int:
     return sum(parts)
 
 
+def extract_potential_gears(line: str) -> List[int]:
+    return [m.start() for m in re.finditer(r"(\*)", line)]
+
+
+def range_overlaps(start1, end1, start2, end2):
+    """Does the range (start1, end1) overlap with (start2, end2)?"""
+    return end1 >= start2 and end2 >= start1
+
+
+def check_for_numbers(line: str, pos: int) -> List[int]:
+    start = pos - 1 if pos > 0 else pos
+    end = pos + 1 if pos < len(line) - 1 else pos
+    numbers = [
+        (int(m.group()), m.start(), m.end()) for m in re.finditer(r"(\d+)", line)
+    ]
+    return [num[0] for num in numbers if range_overlaps(num[1], num[2], start, end)]
+
+
+def is_gear(schematic: List[str], line_number: int, pos: int) -> Tuple[int, int] | None:
+    adjacent_numbers = []
+    if line_number > 0:
+        adjacent_numbers.extend(
+            check_for_numbers(line=schematic[line_number - 1], pos=pos)
+        )
+    if line_number < len(schematic) - 1:
+        adjacent_numbers.extend(
+            check_for_numbers(line=schematic[line_number + 1], pos=pos)
+        )
+    adjacent_numbers.extend(check_for_numbers(line=schematic[line_number], pos=pos))
+    ret = tuple(adjacent_numbers) if len(adjacent_numbers) == 2 else None
+    return ret
+
+
 def solve_2(input: List[str]) -> int:
-    return 0
+    lines = [extract_potential_gears(line) for line in input]
+    gears = []
+    for i, line in enumerate(lines):
+        for gear in line:
+            if is_adjacent_to_symbol(input, i, part):
+                parts.append(int(part[0]))
+    return sum(parts)
 
 
 def main():
-    with open(f"{INPUT_DIR}/3_1_input.txt") as f:
+    with open(f"{INPUT_DIR}/3_1_input.txt", "r") as f:
         x = f.readlines()
         print(f"Part 1: {solve_1(x)}")
         print(f"Part 2: {solve_2(x)}")
--- tests/test_3.py
+++ tests/test_3.py
@@ -6,7 +6,7 @@ from aoc.day_3 import *
 
 @pytest.fixture
 def test_input_1():
-    with open(f"{INPUT_DIR}/3_1_test.txt") as f:
+    with open(f"{INPUT_DIR}/3_1_test.txt", "r") as f:
         return f.readlines()
 
 
@@ -45,3 +45,42 @@ def test_is_adjacent_to_symbol():
 
 def test_solve_1(test_input_1):
     assert solve_1(test_input_1) == 4361
+
+
+def test_extract_gears():
+    assert extract_potential_gears("...*...755....$.*.....664.598..") == [3, 16]
+
+
+@pytest.mark.parametrize(
+    "start1,end1,start2,end2,expected",
+    [
+        (13, 15, 11, 14, True),
+        (13, 15, 15, 18, True),
+        (13, 15, 1, 4, False),
+        (4, 6, 1, 4, True),
+    ],
+)
+def test_range_overlaps(start1, end1, start2, end2, expected):
+    assert range_overlaps(start1, end1, start2, end2) == expected
+
+
+@pytest.mark.parametrize(
+    "line,pos,expected",
+    [
+        (".755..$.*..664.598..", 0, [755]),
+        (".755..$.*..664.598..", 2, [755]),
+        (".755..$.*..664.598..", 8, []),
+        (".755..$.*..664.598..", 5, [755]),
+        (".755..$.*..664.598..", 14, [664, 598]),
+    ],
+)
+def test_check_for_numbers(line, pos, expected):
+    assert check_for_numbers(line, pos) == expected
+
+
+@pytest.mark.parametrize(
+    "line,line_no,pos,expected",
+    [(["467..114..", "...*......", "..35..633."], 1, 3, (467, 35))],
+)
+def test_is_gear(line, line_no, pos, expected):
+    assert is_gear(line, line_no, pos) == expected
