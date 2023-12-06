3dae89a43f046a597d4ca74102920af644e2d8c3
René Gentzen <rene.gentzen@mittelstand.ai>
2023-12-06 12:42:43 +0000

    Added template file

---
+++ src/aoc/day_x.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from aoc import INPUT_DIR
+
+
+def solve_1(input: List[str]) -> int:
+    return 0
+
+
+def solve_2(input: List[str]) -> int:
+    return 0
+
+
+def main():
+    with open(f"{INPUT_DIR}/x_1_input.txt") as f:
+        x = f.readlines()
+        print(f"Part 1: {solve_1(x)}")
+        print(f"Part 2: {solve_2(x)}")
+
+
+if __name__ == "__main__":
+    main()
